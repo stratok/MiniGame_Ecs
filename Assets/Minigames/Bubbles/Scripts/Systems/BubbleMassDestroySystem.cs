614316c20a20441ce1243fd5fc1a75568426e1b8
Stas <dos@ambidexter.io>
2019-08-05 11:48:05 +0300

    Added files

---
+++ Assets/Minigames/Bubbles/Scripts/Systems/BubbleMassDestroySystem.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Entities;
+
+namespace Minigames.Bubbles
+{
+    public class BubbleMassDestroySystem : ComponentSystem
+    {
+        protected override void OnUpdate()
+        {
+            Entities.WithAll<StopGame>().ForEach((Entity entity) =>
+            {
+                Entities.WithAll<BubbleComponent>().ForEach((Entity babbleEntity) =>
+                {
+                    PostUpdateCommands.DestroyEntity(babbleEntity);
+                });
+                PostUpdateCommands.DestroyEntity(entity);
+            });
+        }
+    }
+}
